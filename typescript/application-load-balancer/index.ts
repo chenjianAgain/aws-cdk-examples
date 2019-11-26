45396cb652668906b7e862362489638723acef6c
Joseph Chenkie <josephchenjian@icloud.com>
2019-11-26 09:46:40 +0800

    Update index.ts

--- typescript/application-load-balancer/index.ts
+++ typescript/application-load-balancer/index.ts
@@ -9,32 +9,43 @@ class LoadBalancerStack extends cdk.Stack {
     super(app, id);
 
     const vpc = new ec2.Vpc(this, 'VPC');
+    
+    const cfnLC = new autoscaling.CfnLaunchConfiguration(this, "Devops", {
+            //imageId: 'ami-0cca8c385ef630f54',
+            machineImage: new ec2.AmazonLinuxImage(),
+            instanceType: 't2.micro',
+            //iamInstanceProfile: 'arn:aws:iam::476985428237:instance-profile/EC2AccessCfn',
+            //keyName: 'damo_api',
+            //securityGroups: ['sg-03d294231c1b99316'],
+            //launchConfigurationName: "HistoryRetrieverInstance"	
+        });
 
     const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinuxImage(),
+      launchConfigurationName: cfnLC.ref,
+      //instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      //machineImage: new ec2.AmazonLinuxImage(),
     });
 
-    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
-      vpc,
-      internetFacing: true
-    });
+//     const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+//       vpc,
+//       internetFacing: true
+//     });
 
-    const listener = lb.addListener('Listener', {
-      port: 80,
-    });
+//     const listener = lb.addListener('Listener', {
+//       port: 80,
+//     });
 
-    listener.addTargets('Target', {
-      port: 80,
-      targets: [asg]
-    });
+//     listener.addTargets('Target', {
+//       port: 80,
+//       targets: [asg]
+//     });
 
-    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+//     listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
 
-    asg.scaleOnRequestCount('AModestLoad', {
-      targetRequestsPerSecond: 1
-    });
+//     asg.scaleOnRequestCount('AModestLoad', {
+//       targetRequestsPerSecond: 1
+//     });
   }
 }
 
